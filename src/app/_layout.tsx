d58b5c53f3cd24b8138eced228d27d54c3f58065
Viveq Kanhai <viveqmisre@outlook.com>
2026-07-10 13:16:08 -0300

    tailwind themes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,28 @@
 import { Stack } from "expo-router";
+import { useColorScheme } from "nativewind";
+import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../../global.css";
 import { AuthProvider } from "../context/AuthContext";
 
+function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const { setColorScheme } = useColorScheme();
+
+  useEffect(() => {
+    setColorScheme("dark");
+  }, []);
+
+  return <>{children}</>;
+}
+
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <AuthProvider>
-        <Stack screenOptions={{ headerShown: false }} />
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <Stack screenOptions={{ headerShown: false }} />
+        </AuthProvider>
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+
   presets: [require("nativewind/preset")],
+
+  darkMode: "class",
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-dark": "hsl(var(--bg-dark) / <alpha-value>)",
+        bg: "hsl(var(--bg) / <alpha-value>)",
+        "bg-light": "hsl(var(--bg-light) / <alpha-value>)",
+
+        text: "hsl(var(--text) / <alpha-value>)",
+        "text-muted": "hsl(var(--text-muted) / <alpha-value>)",
+
+        border: "hsl(var(--border) / <alpha-value>)",
+        highlight: "hsl(var(--highlight) / <alpha-value>)",
+      },
+    },
   },
   plugins: [],
 };
